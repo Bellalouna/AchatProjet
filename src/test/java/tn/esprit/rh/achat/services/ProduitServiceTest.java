7d244b95af20b80837ed1b5cdd4797dc4361c59e
Bellalouna <marwa.bellalouna@esprit.tn>
2023-03-04 15:34:07 +0100

    test de stock et produit

--- src/test/java/tn/esprit/rh/achat/services/ProduitServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/services/ProduitServiceTest.java
@@ -1,26 +1,94 @@
 package tn.esprit.rh.achat.services;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.rh.achat.entities.Produit;
-import tn.esprit.rh.achat.services.IProduitService;
+import tn.esprit.rh.achat.repositories.ProduitRepository;
+import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ExtendWith(MockitoExtension.class)
 public class ProduitServiceTest {
 
+    
     @Autowired
-    IProduitService ps;
+    private ProduitRepository produitRepository;
+    
+    @Mock
+    private IProduitService ps;    
 
-    @Test
+    /*@Test
     @Order(1)
-    public void testRetrieveAllUsers() {
-        List<Produit> listProduits = ps.retrieveAllProduits();
-        Assertions.assertEquals(0, listProduits.size());
+    public void testRetrieveAllProduits() {
+        List<Produit> expected = Arrays.asList(
+            new Produit("p1", 1),
+            new Produit("p2", 2)
+        );
+
+        when(ps.retrieveAllProduits()).thenReturn(expected);
+        List<Produit> actual = ps.retrieveAllProduits();
+        Assertions.assertEquals(expected, actual);
+    }*/
+
+
+    @Order(2)
+    @Test
+    public void testAddProduit() {
+        Produit expected = new Produit();
+        Produit result = ps.addProduit(expected);
+        Assertions.assertEquals(expected, result);
     }
+    
+    
+    @Order(3)
+    @Test
+    public void testUpdateProduit() {
+        Produit p = new Produit();
+        Produit updatedProduit = ps.updateProduit(p);
+        Assertions.assertEquals(p, updatedProduit);
+    }
+    
+    
+    @Order(4)
+    @Test
+    public void testretrieveProduit() {
+        Long produitId = 2L;
+        Produit produit = ps.retrieveProduit(produitId);
+        Assertions.assertNotNull(produit);
+    }
+    
+    
+    
+    @Order(5)
+    @Test
+    public void testAssignProduitToStock() {
+        Long idProduit = 4L;
+        Long idStock = 1L;
+        ps.assignProduitToStock(idProduit, idStock);
+        Produit produit = produitRepository.findById(idProduit).get();
+        Assertions.assertEquals(produit.getStock().getIdStock(), idStock);
+    }
+
+
+    
+    @Order(6)
+    @Test
+    public void testDeleteProduit() {
+    Long produitId = 44L;
+    ps.deleteProduit(produitId);
+    Optional<Produit> produit = produitRepository.findById(produitId);
+    Assertions.assertFalse(produit.isPresent());
+    }
+
 
 
 }
---
+++ src/test/java/tn/esprit/rh/achat/services/StockServiceTest.java
@@ -0,0 +1,85 @@
+package tn.esprit.rh.achat.services;
+
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import tn.esprit.rh.achat.entities.Produit;
+import tn.esprit.rh.achat.entities.Stock;
+import tn.esprit.rh.achat.repositories.StockRepository;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class StockServiceTest {
+	
+	@Autowired
+	IStockService ss;
+	
+	@Autowired
+	private StockRepository stockRepository;
+	
+	@Order(1)
+	@Test
+	public void testRetrieveAllStocks() {
+		List<Stock> lisStocks = ss.retrieveAllStocks();
+		Assertions.assertEquals(5, lisStocks.size());
+	}
+	
+	@Order(2)
+	//@Test
+	public void testAddStock() {
+		Stock expected = new Stock();
+		Stock result = ss.addStock(expected);
+		Assertions.assertEquals(expected, result);
+	}
+	
+	@Order(3)
+	@Test
+	public void testUpdateStock() {
+		Stock s = new Stock();
+		Stock updateStock = ss.updateStock(s);
+		Assertions.assertEquals(s, updateStock);
+	}
+	
+	@Order(4)
+	//@Test
+	public void testDeleteStock() {
+		Long stockId = 12L;
+		ss.deleteStock(stockId);
+		Optional<Stock> stock = stockRepository.findById(stockId);
+	    Assertions.assertFalse(stock.isPresent());
+	}
+	
+	@Order(5)
+	@Test
+	public void testRetrieveStock() {
+		Long stockId = 1L;
+		Stock stock = ss.retrieveStock(stockId);
+		Assertions.assertNotNull(stock);
+	}
+	
+	@Order(6)
+	@Test
+	public void testRetrieveStatusStock() {
+	  List<Stock> stocks = new ArrayList<>();
+	  Stock stock1 = new Stock("stock1", 10, 5);
+	  Stock stock2 = new Stock("stock2", 20, 15);
+	  stocks.add(stock1);
+	  stocks.add(stock2);
+
+	  when(stockRepository.retrieveStatusStock()).thenReturn(stocks);
+	  String result = ss.retrieveStatusStock();
+	  Assertions.assertNotNull(result);
+	}	
+
+}
